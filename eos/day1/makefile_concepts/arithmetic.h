57be30cd1f2742baa02acabc7c58174463a373b7
hiyagit <hiya131pnd@gmail.com>
2022-12-07 11:22:03 +0530

    Added header files (with their respective library) named arith.h and arithmetic.h which contains add,sub,mul,div functions

---
+++ eos/day1/makefile_concepts/arithmetic.h
@@ -0,0 +1,22 @@
+
+/*****************************************************************************************
+
+Function Name - add
+Parameter 01 - Integer type
+Parameter 02 - Integer type
+Return Type - Integer
+Desciption - This function takes two arguments and performs arithmetic addition on them. 
+
+******************************************************************************************/
+int add(int, int);
+
+/*****************************************************************************************
+
+Function Name - sub
+Parameter 01 - Integer type
+Parameter 02 - Integer type
+Return Type - Integer
+Desciption - This function takes two arguments and performs arithmetic subtraction on them. 
+
+******************************************************************************************/
+int sub(int, int);
